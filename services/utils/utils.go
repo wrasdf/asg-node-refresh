4f830ad136513affedb03a6a25dc110b81d86731
wrasdf <452784+wrasdf@users.noreply.github.com>
2021-01-27 00:18:53 +1100

    code format

--- services/utils/utils.go
+++ services/utils/utils.go
@@ -6,8 +6,8 @@ import (
 
 func StringToInt64(s string) (int64, error) {
   result, err := strconv.ParseInt(s, 10, 64)
-	if err == nil {
-		return result, nil
+	if err != nil {
+		return 0, err
 	}
-  return 0, err
+  return result, nil
 }
